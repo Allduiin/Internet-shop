a5d487259ac485fa1d1e61e55c276d1ec3ca564d
Allduiin <vovapro01@gmail.com>
2020-04-23 12:07:39 +0300

    removed all not ItemDaos, remade Optional

---
+++ src/main/java/internetshop/service/ItemService.java
@@ -0,0 +1,17 @@
+package internetshop.service;
+
+import internetshop.model.Item;
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemService {
+    Item create(Item item);
+
+    Item getById(Long id);
+
+    List<Item> getAll();
+
+    Item update(Item item);
+
+    boolean delete(Long id);
+}
