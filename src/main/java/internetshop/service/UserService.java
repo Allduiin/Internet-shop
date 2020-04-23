b58ce6b69c1cabbbeedff7f49da0641f5ea8450d
Allduiin <vovapro01@gmail.com>
2020-04-23 17:53:59 +0300

    Added Order and changed main method

--- src/main/java/internetshop/service/UserService.java
+++ src/main/java/internetshop/service/UserService.java
@@ -1,18 +1,16 @@
-package internetshop.dao;
+package internetshop.service;
 
 import internetshop.model.User;
 import java.util.List;
-import java.util.Optional;
 
-public interface UserDao {
+public interface UserService {
     User create(User user);
 
-    Optional<User> get(Long id);
+    User get(Long id);
 
     List<User> getAll();
 
     User update(User user);
 
     boolean delete(Long id);
-
 }
