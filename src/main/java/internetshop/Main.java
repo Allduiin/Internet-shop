b2dd65410498ab149616bff7197d825b127f3c53
Allduiin <vovapro01@gmail.com>
2020-04-24 11:27:54 +0300

    Create Order changed to clone products

--- src/main/java/internetshop/Main.java
+++ src/main/java/internetshop/Main.java
@@ -9,7 +9,7 @@ import internetshop.service.OrderService;
 import internetshop.service.ProductService;
 import internetshop.service.ShoppingCartService;
 import internetshop.service.UserService;
-import java.util.*;
+import java.util.List;
 
 public class Main {
     private static Injector injector = Injector.getInstance("internetshop");
@@ -17,7 +17,8 @@ public class Main {
     public static void main(String[] args) {
         ProductService productService = (ProductService) injector.getInstance(ProductService.class);
         UserService userService = (UserService) injector.getInstance(UserService.class);
-        ShoppingCartService shoppingCartService = (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+        ShoppingCartService shoppingCartService =
+                (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
         OrderService orderService = (OrderService) injector.getInstance(OrderService.class);
         initializeDb(productService, userService, shoppingCartService, orderService);
 
@@ -43,7 +44,8 @@ public class Main {
     }
 
     private static void initializeDb(ProductService productService, UserService userService,
-                                     ShoppingCartService shoppingCartService, OrderService orderService) {
+                                     ShoppingCartService shoppingCartService,
+                                     OrderService orderService) {
         Product product1 = new Product("iPhone", 1000);
         Product product2 = new Product("Nokia", 100);
         Product product3 = new Product("Sumsung", 500);
@@ -51,24 +53,23 @@ public class Main {
         productService.create(product2);
         productService.create(product3);
 
-        User user1 = new User("Pasha","15fe32d5f");
-        User user2 = new User("Vasia","8734t");
-        User user3 = new User("Petia","df15");
+        User user1 = new User("Pasha", "15fe32d5f");
+        User user2 = new User("Vasia", "8734t");
+        User user3 = new User("Petia", "df15");
         userService.create(user1);
         userService.create(user2);
         userService.create(user3);
 
-
         ShoppingCart shoppingCart1 = new ShoppingCart(user1);
         shoppingCartService.create(shoppingCart1);
-        shoppingCartService.addProduct(shoppingCart1,product1);
-        shoppingCartService.addProduct(shoppingCart1,product2);
+        shoppingCartService.addProduct(shoppingCart1, product1);
+        shoppingCartService.addProduct(shoppingCart1, product2);
 
         ShoppingCart shoppingCart2 = new ShoppingCart(user2);
         shoppingCartService.create(shoppingCart2);
-        shoppingCartService.addProduct(shoppingCart2,product2);
-        shoppingCartService.addProduct(shoppingCart2,product3);
+        shoppingCartService.addProduct(shoppingCart2, product2);
+        shoppingCartService.addProduct(shoppingCart2, product3);
 
-        orderService.completeOrder(Arrays.asList(product1,product2), shoppingCart1.getUser());
+        orderService.completeOrder(shoppingCart1.getProducts(), shoppingCart1.getUser());
     }
 }
