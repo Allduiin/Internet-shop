70b46be0c9772f75b5d8773ec642d73827e31881
Allduiin <vovapro01@gmail.com>
2020-04-28 19:29:20 +0300

    Added user controllers, product controllers, service controllers
    (cherry picked from commit 55aa2d2f67f4400835953e0e534564f054ff1719)

---
+++ src/main/java/internetshop/controllers/product/AddProductController.java
@@ -0,0 +1,31 @@
+package internetshop.controllers.product;
+
+import internetshop.lib.Injector;
+import internetshop.model.Product;
+import internetshop.service.ProductService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddProductController extends HttpServlet {
+    private static final Injector INJECTOR = Injector.getInstance("internetshop");
+    ProductService productService = (ProductService) INJECTOR.getInstance(ProductService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/products/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String price = req.getParameter("price");
+        productService.create(new Product(name, Long.parseLong(price)));
+
+        resp.sendRedirect(req.getContextPath() + "/");
+    }
+}
