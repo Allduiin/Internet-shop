7d7aa7adb43c3126534a70ddb32bacbe8ec8fff3
Volodymyr Protsenko <vovapro01@gmail.com>
2020-05-09 19:40:34 +0300

    Added ConnectionUtil, changed ProductDaoImpl add, getByUser

--- src/main/java/internetshop/dao/impl/ProductDaoImpl.java
+++ src/main/java/internetshop/dao/impl/ProductDaoImpl.java
@@ -4,23 +4,54 @@ import internetshop.dao.ProductDao;
 import internetshop.lib.Dao;
 import internetshop.model.Product;
 import internetshop.storage.Storage;
+import internetshop.util.ConnectionUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
 @Dao
 public class ProductDaoImpl implements ProductDao {
+    Connection connection = ConnectionUtil.getConnection();
+
     @Override
     public Product create(Product product) {
+        String query = "INSERT INTO products ( name, price) VALUES (?,?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1,product.getName());
+            statement.setDouble(2,product.getPrice());
+            statement.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         Storage.addProduct(product);
         return product;
     }
 
     @Override
     public Optional<Product> getById(Long id) {
-        return Storage.products.stream()
-                .filter(i -> i.getId().equals(id))
-                .findFirst();
+        String query = "SELECT * FROM products WHERE product_id = ?";
+        Product product = new Product(null, 0);
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setLong(1,id);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Long productId = resultSet.getLong("product_id");
+                String name = resultSet.getString("name");
+                double price = resultSet.getDouble("price");
+                product.setId(productId);
+                product.setName(name);
+                product.setPrice(price);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't read result of statment", e);
+        }
+        return Optional.of(product);
     }
 
     @Override
