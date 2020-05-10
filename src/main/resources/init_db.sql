169122f03deeb3e38b4a035f15e1d1aeea3eacc6
Volodymyr Protsenko <vovapro01@gmail.com>
2020-05-10 14:07:13 +0300

    added init_db.sql

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA `internet_shop` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `internet_shop`.`users` (
+  `user_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(225) NOT NULL,
+  `password` VARCHAR(225) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `user name_UNIQUE` (`username` ASC) VISIBLE);
+
+INSERT INTO `internet_shop`.`users` (`username`, `password`) VALUES ('bob', '1');
+
+CREATE TABLE `internet_shop`.`products` (
+  `product_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(225) NOT NULL,
+  `price` DOUBLE NOT NULL,
+  PRIMARY KEY (`product_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
