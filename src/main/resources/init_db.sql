7a3ee2525bae5d009740aa6a2590b715b4555360
Volodymyr Protsenko <vovapro01@gmail.com>
2020-05-14 17:53:44 +0300

    updated init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,91 +1,65 @@
 CREATE SCHEMA `internet_shop` DEFAULT CHARACTER SET utf8;
 
-CREATE TABLE `internet_shop`.`users`
-(
-    `user_id`  BIGINT(11)   NOT NULL AUTO_INCREMENT,
-    `login`    VARCHAR(225) NOT NULL,
-    `password` VARCHAR(225) NOT NULL,
-    PRIMARY KEY (`user_id`),
-    UNIQUE INDEX `user name_UNIQUE` (`login` ASC) VISIBLE
-);
+CREATE TABLE `orders` (
+                          `order_id` bigint NOT NULL AUTO_INCREMENT,
+                          `user_id` bigint DEFAULT NULL,
+                          PRIMARY KEY (`order_id`),
+                          KEY `orders_users_fk_idx` (`user_id`),
+                          CONSTRAINT `orders_users_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
-INSERT INTO `internet_shop`.`users` (`login`, `password`)
-VALUES ('bob', '1');
+CREATE TABLE `orders_products` (
+                                   `order_id` bigint NOT NULL,
+                                   `product_id` bigint NOT NULL,
+                                   KEY `orders_fk_idx` (`order_id`),
+                                   KEY `products_fk_idx` (`product_id`),
+                                   CONSTRAINT `orders_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+                                   CONSTRAINT `products_fk` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `internet_shop`.`products`
-(
-    `product_id` BIGINT(11)   NOT NULL AUTO_INCREMENT,
-    `name`       VARCHAR(225) NOT NULL,
-    `price`      DOUBLE       NOT NULL,
-    PRIMARY KEY (`product_id`)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `products` (
+                            `product_id` bigint NOT NULL AUTO_INCREMENT,
+                            `name` varchar(225) NOT NULL,
+                            `price` double NOT NULL,
+                            PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `internet_shop`.`orders`
-(
-    `order_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-    `user_id`  BIGINT(11) NULL,
-    PRIMARY KEY (`order_id`),
-    INDEX `orders_users_fk_idx` (`user_id` ASC) VISIBLE,
-    CONSTRAINT `orders_users_fk`
-        FOREIGN KEY (`user_id`)
-            REFERENCES `internet_shop`.`users` (`user_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `roles` (
+                         `role_id` bigint NOT NULL,
+                         `name` varchar(225) NOT NULL,
+                         PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `internet_shop`.`orders_products`
-(
-    `product_id` BIGINT(11) NOT NULL,
-    `order_id`   BIGINT(11) NOT NULL,
-    INDEX `orders_id_fk_idx` (`order_id` ASC) VISIBLE,
-    INDEX `products_id_fk_idx` (`product_id` ASC) VISIBLE,
-    CONSTRAINT `orders_id_fk`
-        FOREIGN KEY (`order_id`)
-            REFERENCES `internet_shop`.`orders` (`order_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `products_id_fk`
-        FOREIGN KEY (`product_id`)
-            REFERENCES `internet_shop`.`products` (`product_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `shopping_carts` (
+                                  `cart_id` bigint NOT NULL AUTO_INCREMENT,
+                                  `user_id` bigint NOT NULL,
+                                  PRIMARY KEY (`cart_id`,`user_id`),
+                                  KEY `shoppingcart_user_fk_idx` (`user_id`),
+                                  CONSTRAINT `shoppingcart_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `internet_shop`.`shopping_carts`
-(
-    `shopping_cart_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-    `user_id`         BIGINT(11) NOT NULL,
-    PRIMARY KEY (`shopping_cart_id`, `user_id`),
-    INDEX `shoppingcart_user_fk_idx` (`user_id` ASC) VISIBLE,
-    CONSTRAINT `shoppingcart_user_fk`
-        FOREIGN KEY (`user_id`)
-            REFERENCES `internet_shop`.`users` (`user_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-);
+CREATE TABLE `shopping_carts_products` (
+                                           `cart_id` bigint NOT NULL,
+                                           `product_id` bigint NOT NULL,
+                                           KEY `shopping_cart_fk_idx` (`cart_id`),
+                                           KEY `product_fk_idx` (`product_id`),
+                                           CONSTRAINT `product_fk` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
+                                           CONSTRAINT `shopping_cart_fk` FOREIGN KEY (`cart_id`) REFERENCES `shopping_carts` (`cart_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `internet_shop`.`shopping_carts_products`
-(
-    `cart_id`    BIGINT(11) NOT NULL,
-    `product_id` BIGINT(11) NOT NULL,
-    INDEX `shopping_cart_fk_idx` (`cart_id` ASC) VISIBLE,
-    INDEX `product_fk_idx` (`product_id` ASC) VISIBLE,
-    CONSTRAINT `shopping_cart_fk`
-        FOREIGN KEY (`cart_id`)
-            REFERENCES `internet_shop`.`shopping_carts` (`shoppingcart_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `product_fk`
-        FOREIGN KEY (`product_id`)
-            REFERENCES `internet_shop`.`products` (`product_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-);
+CREATE TABLE `users` (
+                         `user_id` bigint NOT NULL AUTO_INCREMENT,
+                         `login` varchar(225) NOT NULL,
+                         `password` varchar(225) NOT NULL,
+                         PRIMARY KEY (`user_id`),
+                         UNIQUE KEY `user name_UNIQUE` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8;
 
-
+CREATE TABLE `users_roles` (
+                               `user_id` bigint NOT NULL,
+                               `role_id` bigint NOT NULL,
+                               KEY `users_fk_idx` (`user_id`),
+                               KEY `role_fk_idx` (`role_id`),
+                               CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
+                               CONSTRAINT `users_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
